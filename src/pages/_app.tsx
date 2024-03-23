4a83b232ba393664d78bd3cbe496d1d01cce6e08
Hanseok Oh <hanseokoh@Hanseoks-Mac-mini.local>
2024-03-23 12:15:43 +0900

    Init with t3 starter

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { type AppType } from "next/app";
+import { Inter } from "next/font/google";
+
+import { api } from "@/utils/api";
+
+import "@/styles/globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
+};
+
+export default api.withTRPC(MyApp);
