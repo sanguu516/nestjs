73d66f95a22bb4343a544b3392238acdc54c375e
hanseok513 <hanseok513@gmail.com>
2024-03-25 21:18:08 +0900

    Loose eslint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,6 +36,10 @@ const config = {
         },
       },
     ],
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
   },
 }
 module.exports = config
